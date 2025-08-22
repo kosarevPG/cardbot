c176a1b8c2dce0be525e6b56d8c801aa71735160
Pavel Kosarev <kosarev.pg@gmail.com>
2025-08-22 13:19:52 +0300

    Fix ozon_sync analytics payload: dimension list

--- modules/ozon_sync.py
+++ modules/ozon_sync.py
@@ -132,7 +132,7 @@ class OzonDataSync:
                     "date_from": date_from,
                     "date_to": date_to,
                     "metrics": ["ordered_units", "revenue"],
-                    "dimension": "product_id",
+                    "dimension": ["product_id"],
                     "filters": [
                         {
                             "key": "product_id",
